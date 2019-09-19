b3ac8cebd00dda21ccde2a9555198c93ef33c7e2
Tom Gowan <s3549186@student.rmit.edu.au>
2019-09-19 17:47:12 +1000

    generated code

--- zomes/hello/code/src/lib.rs
+++ zomes/hello/code/src/lib.rs
@@ -29,20 +29,12 @@ use hdk::holochain_persistence_api::{
 };
 
 use hdk_proc_macros::zome;
-
-// see https://developer.holochain.org/api/0.0.18-alpha1/hdk/ for info on using the hdk library
-
-// This is a sample zome that defines an entry type "MyEntry" that can be committed to the
-// agent's chain via the exposed function create_my_entry
-
-#[derive(Serialize, Deserialize, Debug, DefaultJson,Clone)]
-pub struct Person{
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
+pub struct Person {
     name: String,
 }
-
 #[zome]
 mod hello_zome {
-
     #[init]
     fn init() {
         Ok(())
@@ -52,37 +44,35 @@ mod hello_zome {
     pub fn validate_agent(validation_data: EntryValidationData<AgentId>) {
         Ok(())
     }
+  /* --- Lines omitted -- */
+  #[zome_fn("hc_public")]
+  fn hello_holo() -> ZomeApiResult<String> {
+      Ok("Hello Holo".into())
+  }
 
-    #[entry_def]
-    fn person_entry_def() -> ValidatingEntryType {
-        entry!(
-            name: "person",
-            description: "Person to say hello to",
-            sharing: Sharing::Private,
-            validation_package: || {
-                hdk::ValidationPackageDefinition::Entry
-            },
-            validation: | _validation_data: hdk::EntryValidationData<Person>| {
-                Ok(())
-            }
-        )
-    }
-
-    #[zome_fn("hc_public")]
-    pub fn create_person(person: Person) -> ZomeApiResult<Address> {
-        let entry = Entry::App("person".into(), person.into());
-        let address = hdk::commit_entry(&entry)?;
-        Ok(address)
-    }
-
-    #[zome_fn("hc_public")]
-    fn retrieve_person(address: Address) -> ZomeApiResult<Option<Entry>> {
-        hdk::get_entry(&address)
-    }
-
-    #[zome_fn("hc_public")]
-    fn hello_holo() -> ZomeApiResult<String> {
-        Ok("Hello Holo".into())
-    }
-
+  // <---- Add the following lines here.
+#[entry_def]
+fn person_entry_def() -> ValidatingEntryType {
+    entry!(
+        name: "person",
+        description: "Person to say hello to",
+        sharing: Sharing::Private,
+        validation_package: || {
+            hdk::ValidationPackageDefinition::Entry
+        },
+        validation: | _validation_data: hdk::EntryValidationData<Person>| {
+            Ok(())
+        }
+    )
+}
+#[zome_fn("hc_public")]
+pub fn create_person(person: Person) -> ZomeApiResult<Address> {
+    let entry = Entry::App("person".into(), person.into());
+    let address = hdk::commit_entry(&entry)?;
+    Ok(address)
+}
+#[zome_fn("hc_public")]
+fn retrieve_person(address: Address) -> ZomeApiResult<Option<Entry>> {
+    hdk::get_entry(&address)
+}
 }
